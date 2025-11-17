fa641bcaa7268c0db556ae90c05602418366c57e
halohelpbomb <h61875168@gmail.com>
2025-11-17 13:07:59 +0300

    Update halo.java

--- project-root/auth-module/halo.java
+++ project-root/auth-module/halo.java
@@ -1,11 +1,16 @@
 /////////////////////
 package project-root.auth-module.halo
 
-public class auth {
-  public void entire(int idk){
+private static class auth {
+  private void entire(int idk){
     
   if(idk == (int)(Math.random()*6))System.out.println("сделаем вид тут настоящая система аутентификации")
 
     }
+
+  public static boolean checkValid(int clientid) {
+if(clientid == (int)(Math.random()*777)) return true // только первые 777 пользователей смогут хотя бы  пользоваться приложением
+  else return false // либо кому-то просто не повезло с RNG
+  }
 
 }
