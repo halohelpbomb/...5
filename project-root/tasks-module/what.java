2d0ad5ba7f0ec7c974b6614b922139622fd88ad0
halohelpbomb <h61875168@gmail.com>
2025-11-17 13:03:14 +0300

    Update and rename what.cpp to what.java

---
+++ project-root/tasks-module/what.java
@@ -0,0 +1,14 @@
+//\\\
+package project-root.tasks-module.what
+import project-root.tasks-module.auth
+  public class tasks {
+  
+public void start(int clientid) {
+if(auth.checkValid(clientid)) {
+System.out.println("якобы не все подряд таски выполняются. для всех клиентов кроме инвалидов")
+
+}
+}
+
+  
+}
