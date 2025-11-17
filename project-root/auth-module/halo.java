5752e8ab9597a1192515bcbaaaaf40a34062e348
halohelpbomb <h61875168@gmail.com>
2025-11-17 12:57:48 +0300

    Update and rename halo.cpp to halo.java

---
+++ project-root/auth-module/halo.java
@@ -0,0 +1,11 @@
+/////////////////////
+package project-root.auth-module.halo
+
+public class auth {
+  public void entire(int idk){
+    
+  if(idk == (int)(Math.random()*6))System.out.println("сделаем вид тут настоящая система аутентификации")
+
+    }
+
+}
